f45356a40d29d27d15b913355fac86efc3876745
AbhayWalia777 <121758785+AbhayWalia777@users.noreply.github.com>
2024-03-18 12:03:31 +0530

    changes

--- dist/js/ApiSettingPage.js
+++ dist/js/ApiSettingPage.js
@@ -1,83 +1,72 @@
-﻿        $(document).ready(function () {
-            $("#Broker").on('change', function () {
-                var Broker = $("#Broker").val();
-                if (Broker == "ANGEL BROKING") {
-                    $("#apiKeytr").show();
-                    $("#apiSecretKeytr").show();
-                    $("#angelBrokingClientCode").show();
-                    $("#angelBrokingClientPassword").show();
-                    $("#kotakSecurityConsumerKey").hide();
-                    $("#kotakSecurityAccessToken").hide();
-                    $("#kotakSecurityAccessCode").hide();
-                }
-                else if (Broker == "KOTAK SECURITIES")
-                {
-                    $("#apiKeytr").hide();
-                    $("#apiSecretKeytr").hide();
-                    $("#angelBrokingClientCode").hide();
-                    $("#angelBrokingClientPassword").show();
-                    $("#kotakSecurityConsumerKey").show();
-                    $("#kotakSecurityAccessToken").show();
-                    $("#kotakSecurityAccessCode").show();
-                }
-                else if (Broker == "CAPITALS") {
-                    $("#apiKeytr").show();
-                    $("#UserID").hide();
-                    $("#apiSecretKeytr").hide();
-                    $("#angelBrokingClientCode").show();
-                    $("#angelBrokingClientPassword").show();
-                    $("#kotakSecurityConsumerKey").hide();
-                    $("#kotakSecurityAccessToken").hide();
-                    $("#kotakSecurityAccessCode").hide();
-                }
-                else {
-                    $("#apiKeytr").show();
-                    $("#apiSecretKeytr").show();
-                    $("#angelBrokingClientCode").show();
-                    $("#angelBrokingClientCode").hide();
-                    $("#angelBrokingClientPassword").hide();
-                    $("#kotakSecurityConsumerKey").hide();
-                    $("#kotakSecurityAccessToken").hide();
-                    $("#kotakSecurityAccessCode").hide();
-                }
-            });
-            var Broker = $("#Broker").val();
-            if (Broker == "ANGEL BROKING") {
-                $("#apiKeytr").show();
-                $("#apiSecretKeytr").show();
-                $("#angelBrokingClientCode").show();
-                $("#angelBrokingClientPassword").show();
-                $("#kotakSecurityConsumerKey").hide();
-                $("#kotakSecurityAccessToken").hide();
-                $("#kotakSecurityAccessCode").hide();
-            }
-            else if (Broker == "KOTAK SECURITIES") {
-                $("#apiKeytr").hide();
-                $("#apiSecretKeytr").hide();
-                $("#angelBrokingClientCode").hide();
-                $("#angelBrokingClientPassword").show();
-                $("#kotakSecurityConsumerKey").show();
-                $("#kotakSecurityAccessToken").show();
-                $("#kotakSecurityAccessCode").show();
-            }
-            else if (Broker == "CAPITALS") {
-                $("#apiKeytr").show();
-                $("#UserID").hide();
-                $("#apiSecretKeytr").hide();
-                $("#angelBrokingClientCode").show();
-                $("#angelBrokingClientPassword").show();
-                $("#kotakSecurityConsumerKey").hide();
-                $("#kotakSecurityAccessToken").hide();
-                $("#kotakSecurityAccessCode").hide();
-            }
-            else {
-                $("#apiKeytr").show();
-                $("#apiSecretKeytr").show();
-                $("#angelBrokingClientCode").show();
-                $("#angelBrokingClientCode").hide();
-                $("#angelBrokingClientPassword").hide();
-                $("#kotakSecurityConsumerKey").hide();
-                $("#kotakSecurityAccessToken").hide();
-                $("#kotakSecurityAccessCode").hide();
-            }
-        });
+﻿window.onload(function () {
+    var Broker = $("#Broker").val();
+    $("#apiKeytr").show();
+    $("#apiSecretKeytr").show();
+    $("#angelBrokingClientCode").show();
+    $("#angelBrokingClientPassword").show();    
+    $("#kotakSecurityConsumerKey").show();
+    $("#kotakSecurityAccessToken").show();
+    $("#kotakSecurityAccessCode").show();    
+    $("#UserID").show();       
+    if (Broker == "ANGEL BROKING") {
+        $("#kotakSecurityConsumerKey").hide();
+        $("#kotakSecurityAccessToken").hide();
+        $("#kotakSecurityAccessCode").hide();
+    }
+    else if (Broker == "KOTAK SECURITIES") {
+        $("#apiKeytr").hide();
+        $("#apiSecretKeytr").hide();
+        $("#angelBrokingClientCode").hide();
+    }
+    else if (Broker == "CAPITALS") {        
+        $("#UserID").hide();
+        $("#apiSecretKeytr").hide();
+        $("#kotakSecurityConsumerKey").hide();
+        $("#kotakSecurityAccessToken").hide();
+        $("#kotakSecurityAccessCode").hide();
+    }
+    else {
+
+        $("#angelBrokingClientCode").hide();
+        $("#angelBrokingClientPassword").hide();
+        $("#kotakSecurityConsumerKey").hide();
+        $("#kotakSecurityAccessToken").hide();
+        $("#kotakSecurityAccessCode").hide();
+    }
+    $("#Broker").on('change', function () {
+        var Broker = $("#Broker").val();
+        $("#apiKeytr").show();
+        $("#apiSecretKeytr").show();
+        $("#angelBrokingClientCode").show();
+        $("#angelBrokingClientPassword").show();
+        $("#kotakSecurityConsumerKey").show();
+        $("#kotakSecurityAccessToken").show();
+        $("#kotakSecurityAccessCode").show();
+        $("#UserID").show();
+        if (Broker == "ANGEL BROKING") {
+            $("#kotakSecurityConsumerKey").hide();
+            $("#kotakSecurityAccessToken").hide();
+            $("#kotakSecurityAccessCode").hide();
+        }
+        else if (Broker == "KOTAK SECURITIES") {
+            $("#apiKeytr").hide();
+            $("#apiSecretKeytr").hide();
+            $("#angelBrokingClientCode").hide();
+        }
+        else if (Broker == "CAPITALS") {
+            $("#UserID").hide();
+            $("#apiSecretKeytr").hide();
+            $("#kotakSecurityConsumerKey").hide();
+            $("#kotakSecurityAccessToken").hide();
+            $("#kotakSecurityAccessCode").hide();
+        }
+        else {
+
+            $("#angelBrokingClientCode").hide();
+            $("#angelBrokingClientPassword").hide();
+            $("#kotakSecurityConsumerKey").hide();
+            $("#kotakSecurityAccessToken").hide();
+            $("#kotakSecurityAccessCode").hide();
+        }
+    });
+});
